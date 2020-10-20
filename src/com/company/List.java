3dc43459105e5eebf24e3d80438be7ce845b64af
Katya <mak4rovayekat@yandex.ru>
2020-10-20 22:26:14 +0300

    first commit

---
+++ src/com/company/List.java
@@ -0,0 +1,53 @@
+package com.company;
+
+class Node {
+    double value_;
+    Node next_;
+    public Node(double value) {
+        value_ = value;
+        next_ = null;
+    }
+    public Node(double value, Node next) {
+        value_ = value;
+        next_ = next;
+    }
+    public Node() {
+        value_ = 0;
+        next_ = null;
+    }
+}
+
+public class List {
+    private Node header;
+    public List() {
+        header = new Node();
+    }
+    public void pushEnd(double value){
+       Node tmp = header;
+        while (tmp.next_ !=null) {
+            tmp = tmp.next_;
+        }
+        Node element = new Node(value);
+        tmp.next_=element;
+    }
+    public void print(){
+        Node tmp = header.next_;
+        while (tmp !=null){
+            System.out.print(tmp.value_ + " ");
+            tmp = tmp.next_;
+        }
+        System.out.println();
+    }
+    public void remove(int index){
+        int i = 1;
+        Node tmp = header;
+        while (tmp.next_ !=null && i != index){
+            tmp = tmp.next_;
+            i++;
+        }
+        if (tmp.next_ == null)
+            System.out.println("oshibka");
+        else
+            tmp.next_ = tmp.next_.next_;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List a = new List();
+        for(int i = 1; i <= 10; i++) {
+            a.pushEnd(i);
+        }
+        a.print();
+        a.remove(4);
+        a.print();
+        a.remove(15);
+        a.print();
+    }
+}
